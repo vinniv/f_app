98ad634df33d2c7578b7996f390b114d58a3cf1e
vinniv <vinceturk@gmail.com>
2010-11-17 18:54:14 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FApp::Application.config.secret_token = '11d9a951d8ca59213a459204046cba10140b62baedd6cc18cbc0889032a5ed58367a8f027852a4ce61f68daa13b20bfa5539da9e9c18fbc0dde57c9ddd096a71'
